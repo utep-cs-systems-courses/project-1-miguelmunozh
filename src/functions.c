365c9e664f6cfb4b2ba7cfaa469fbf660f8b356d
miguelmunozh <miguelmunozh@outlook.com>
2020-06-28 12:56:22 -0600

    added and tested history part

--- src/functions.c
+++ src/functions.c
@@ -17,22 +17,22 @@ int non_space_char(char c){
   }
 }
 //this returns a pointer to a char, and accepts a pointer to a char
-char *word_start(char *s){
+char *word_start(char *str){
   int i = 0;
   while(1){
     //if the char at s[i] is not a space or tab, then it is the first char and we return it
-    if(non_space_char(s[i])){
-      return &s[i];
+    if(non_space_char(str[i])){
+      return &str[i];
     }
     i++;
   }  
 }
-char *word_terminator(char *s){
+char *word_terminator(char *str){
   int i = 0;
   while(1){ // until the end of the string
     //if the char at position s[i] is a space and the char at position s[i-1] is a char, return it, i++ otherwise
-    if((space_char(s[i])) && (non_space_char(s[i-1]))){
-      return &s[i]; // the address of the last char in the first word, // s[0] actual char
+    if((space_char(str[i])) && (non_space_char(str[i-1]))){
+      return &str[i]; // the address of the last char in the first word, // s[0] actual char
     }
     i++;
   }
@@ -43,13 +43,10 @@ int count_words(char *str){
   int i = 0;
   int words = 0;
   while(1){
-    //if current char is a space/tab
-    if(space_char(str[i])){
-      //if the char before the space is a char then we have a word
-      if(non_space_char(prevChar)){
+    //if current char is a space/tab and one char before that is a char, we have a word
+    if(space_char(str[i]) && non_space_char(prevChar)){
 	words++;
       }
-    }
     prevChar = str[i];
     if(str[i] == '\0'){
       break;
@@ -59,6 +56,83 @@ int count_words(char *str){
   }
   return words;
 }
+
+char *copy_str(char *inStr, short len){
+  char *copy = (char*) malloc((len + 1)*sizeof(char*));   //len + 1 to store the zero terminator
+  int i;
+  for(i = 0; i < len; inStr++, i ++){
+    copy[i] = *inStr;
+  }
+  copy[i] = '\0';
+  return copy;
+}
+char** tokenize(char * str){
+  int total = count_words(str);
+  //allocating memory for the array of tokens
+  char **token = (char**)malloc((total+1) * sizeof(char**));
+  int i;
+  for(i = 0; i < total; i++){
+    //space for each word in the array
+    token[i] = malloc((total+1) * sizeof(char*)); /*if I comment this it still work*/
+
+    //start and end of each word
+    char* start = word_start(str);
+    char* end = word_terminator(start);
+    //we copy the word in the array at position i, witht he length of the word
+    token[i] = copy_str(start, (end - start));
+    //then we update the string to find word start after the end fo the word that we just saved
+    str = word_start(end);
+  }
+  return token;
+  free_tokens(token);
+}
+
+//changed \0 to NULL because i was getting a warning
+void print_tokens(char **tokens){
+  int i = 0;
+  while(tokens[i] != NULL){
+    printf("[%s]\n",tokens[i]);
+    i++;
+  }
+  printf("\n");
+}
+
+void free_tokens(char ** tokens){
+  int i = 0;
+  while(tokens[i] != NULL){
+    free(tokens[i]);
+    i++;
+  }
+  free(tokens);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/main.c
+++ src/main.c
@@ -1,27 +1,80 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "tokenizer.h"
+#include "history.h"
 
 int main(){
+  
+  char in[100];
+  /*bugs when reading input*/
+  /*
+  List *list = init_history();
+
+  int a;
+  int m;
+  while(a != -1){
+    printf("enter 1 to tokenize a string\n      2 to print history\n     -1 to quit\n");
+    scanf("%d",&a);
+    char tmp;
+    scanf("%c",&tmp);
+    
+    if(a == 1){
+      printf("Enter string to tokenize:\n > ");
+      fgets(in,100,stdin);
+   
+      char **tokens = tokenize(in);
+      add_history(list,in);
+      print_tokens(tokens);
+  
+
+    }
+    if(a == 2){
+      printf("\nHistory of strings:\n");
+      print_history(list);
+      printf("\nSelect a string number to print or -1 to quit\n");
+      scanf("%d",&m);
+      
+      print_tokens(tokenize(get_history(list,m)));
+    }
+    
+  }
+  printf("\nBYE!\n");
+
+*/
+  // SAME ERROR THANT WITH ABOVE UI
+
+  //char *in;
+  /*
+    printf("%s", "> ");
+    fgets(in, sizeof(in), stdin);
+    //printf("%s", in);
+
+    char **tokens = tokenize(in);
+    print_tokens(tokens);
+  */
+  
+  
+  
+  char *str = "\ta 1\tooo\t ppp \t";
+  print_tokens(tokenize(str));
+  print_tokens(tokenize("   b 2    "));
+  print_tokens(tokenize("a b v c miguelmunozh"));
+  print_tokens(tokenize("v vvvvv cc c    c\t"));
+  print_tokens(tokenize("v v b"));
+  print_tokens(tokenize(" v v b "));
+
+  print_tokens(tokenize("\tv v o\t"));
 
 
-  printf("%d\n",space_char(' ')); //1
+ List *list = init_history();
+  add_history(list,str);
+  add_history(list,"miguel munoz h");
+  add_history(list,"testing the history part ");
+  print_history(list);
+  // get_history(list,1);    
+  print_tokens(tokenize(get_history(list,2)));
+     
 
-  printf("%d\n",space_char('t')); //0
-
-  printf("%d\n",non_space_char('t')); //1
-
-  printf("%d\n",non_space_char(' ')); //0
-
-  printf("%d\n",count_words(" 1 2 3    4 "));
-
-  /*printf("%c\n",word_start("         this   is    where the   word starts?")); //to return the first letter instead of the address*/
-
-  printf("%p\n",word_terminator("         this   is    where the   word starts?"));
-
-  printf("%p\n",word_start("         this   is    where the   word starts?"));
-
-
-
-  return 0;
+  
+return 0;
 }
